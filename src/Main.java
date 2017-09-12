89f81735daf78ec645856d85d3b6c6f8d90f68fe
sowmyamandapati <31842780+sowmyamandapati@users.noreply.github.com>
2017-09-12 13:54:51 +0530

    Add files via upload
    first commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+	public static void main(String[] args) {
+     Date d=new date();
+ student s=new student();
+   s.student(34,"aaaa bb",d,60);
+s.getId();
+s.setId(45);
+s.getFullName();
+s.setFullName("bbbb");
+s.getBirthDate();
+s.setBirthDate();
+s.getAvgMark();
+s.setAvgMark(62);
+
+     
+
+		
+StudentGroup g=new StuentGroup();
+                     g.getStudents();
+//g.setStudents(Student[]);
+g.getStudent(34);
+g.setStudent("a",24);
+g.addFirst("abab");
+g.addLast("bbbg");
+g.add("aaab",13);
+g.remove("aaaa");
+
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
